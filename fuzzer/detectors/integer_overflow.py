9d54e6c802269a362d487c8b050cb855f1ea0a09
Christof Torres <christoftorres@users.noreply.github.com>
2022-01-19 12:33:33 +0100

    Update integer_overflow.py

--- fuzzer/detectors/integer_overflow.py
+++ fuzzer/detectors/integer_overflow.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from z3 import BitVec
-from utils.utils import convert_stack_value_to_int, convert_stack_value_to_hex
+from z3.z3util import get_vars
+from utils.utils import convert_stack_value_to_int
 
 class IntegerOverflowDetector():
     def __init__(self):
@@ -13,113 +13,45 @@ class IntegerOverflowDetector():
         self.severity = "High"
         self.overflows = {}
         self.underflows = {}
-        self.compiler_value_negation = False
 
-    def detect_integer_overflow(self, mfe, tainted_record, previous_instruction, current_instruction, individual, transaction_index):
-        if previous_instruction and previous_instruction["op"] == "NOT" and current_instruction and current_instruction["op"] == "ADD":
-            self.compiler_value_negation = True
-        # Addition
-        elif previous_instruction and previous_instruction["op"] == "ADD":
+    def detect_integer_overflow(self, previous_instruction, current_instruction, tainted_record):
+        if previous_instruction and previous_instruction["op"] == "ADD":
             a = convert_stack_value_to_int(previous_instruction["stack"][-2])
             b = convert_stack_value_to_int(previous_instruction["stack"][-1])
-            #print(convert_stack_value_to_int(previous_instruction["stack"][-2]))
-            #print(convert_stack_value_to_int(previous_instruction["stack"][-1]))
-            if a + b != convert_stack_value_to_int(current_instruction["stack"][-1]) and not self.compiler_value_negation:
-                #print("!!!!!!!!")
-                #print("!!!!!!!!")
-                #print("addition overflow")
-                #print("!!!!!!!!")
-                #print("!!!!!!!!")
+            c = convert_stack_value_to_int(current_instruction["stack"][-1])
+            if a + b != c:
                 if tainted_record and tainted_record.stack and tainted_record.stack[-1]:
-                    index = ''.join(str(taint) for taint in tainted_record.stack[-1])
-                    if "calldataload" in index or "callvalue" in index:
-                        _function_hash = individual.chromosome[transaction_index]["arguments"][0]
-                        _is_string = False
-                        for _argument_index in [int(a.split("_")[-1]) for a in index.split() if a.startswith("calldataload_"+str(transaction_index)+"_")]:
-                            if individual.generator.interface[_function_hash][_argument_index] == "string":
-                                _is_string = True
-                        if not _is_string:
-                            self.overflows[index] = previous_instruction["pc"]
+                    if get_vars(tainted_record.stack[-1][0]):
+                        self.overflows[previous_instruction["pc"]] = get_vars(tainted_record.stack[-1][0])
 
-        # Multiplication
         elif previous_instruction and previous_instruction["op"] == "MUL":
             a = convert_stack_value_to_int(previous_instruction["stack"][-2])
             b = convert_stack_value_to_int(previous_instruction["stack"][-1])
-            if a * b != convert_stack_value_to_int(current_instruction["stack"][-1]):
-                """print(convert_stack_value_to_int(previous_instruction["stack"][-2]))
-                print(convert_stack_value_to_int(previous_instruction["stack"][-1]))
-                print(convert_stack_value_to_hex(previous_instruction["stack"][-1]))
-                print(convert_stack_value_to_int(current_instruction["stack"][-1]))
-                print(convert_stack_value_to_hex(current_instruction["stack"][-1]))
-                print(" ")"""
-                #print("Multiplication overflow")
+            c = convert_stack_value_to_int(current_instruction["stack"][-1])
+            if a * b != c:
                 if tainted_record and tainted_record.stack and tainted_record.stack[-1]:
-                    index = ''.join(str(taint) for taint in tainted_record.stack[-1])
-                    #print("yes")
-                    if "calldataload" in index or "callvalue" in index:
-                        #print("added")
-                        self.overflows[index] = previous_instruction["pc"]
-                        #tainted_record.stack[-2] = [BitVec("_".join(["overflow", hex(previous_instruction["pc"])]), 256)]
-                        #index = ''.join(str(taint) for taint in tainted_record.stack[-2])
-                        #self.overflows[index] = previous_instruction["pc"]
-                #else:
-                #    print("nope")
-                #print("")
-        # Subtraction
+                    if get_vars(tainted_record.stack[-1][0]):
+                        self.overflows[previous_instruction["pc"]] = get_vars(tainted_record.stack[-1][0])
+
         elif previous_instruction and previous_instruction["op"] == "SUB":
-            a = convert_stack_value_to_int(previous_instruction["stack"][-1])
-            b = convert_stack_value_to_int(previous_instruction["stack"][-2])
-            if a - b != convert_stack_value_to_int(current_instruction["stack"][-1]):
+            a = convert_stack_value_to_int(previous_instruction["stack"][-2])
+            b = convert_stack_value_to_int(previous_instruction["stack"][-1])
+            c = convert_stack_value_to_int(current_instruction["stack"][-1])
+            if a - b != c:
                 if tainted_record and tainted_record.stack and tainted_record.stack[-1]:
-                    index = ''.join(str(taint) for taint in tainted_record.stack[-1])
-                    self.underflows[index] = previous_instruction["pc"]
-                else:
-                    tainted_record = mfe.symbolic_taint_analyzer.get_tainted_record(index=-1)
-                    if tainted_record:
-                        tainted_record.stack[-2] = [BitVec("_".join(["underflow", hex(previous_instruction["pc"])]), 256)]
-                        index = ''.join(str(taint) for taint in tainted_record.stack[-2])
-                        self.underflows[index] = previous_instruction["pc"]
+                    if get_vars(tainted_record.stack[-1][0]):
+                        self.underflows[previous_instruction["pc"]] = get_vars(tainted_record.stack[-1][0])
 
-        # Check if overflow flows into storage
         if current_instruction and current_instruction["op"] == "SSTORE":
-            #print("sstore")
-            if tainted_record and tainted_record.stack and tainted_record.stack[-2]: # Storage value
-                index = ''.join(str(taint) for taint in tainted_record.stack[-2])
-                #print("sstore index")
-                #print(index)
-
-                if index in self.overflows:
-                    return self.overflows[index], "overflow"
-                if index in self.underflows:
-                    return self.underflows[index], "underflow"
-        # Check if overflow flows into call
-        elif current_instruction and current_instruction["op"] == "CALL":
-            if tainted_record and tainted_record.stack and tainted_record.stack[-3]: # Call value
-                #print(tainted_record.stack)
-                index = ''.join(str(taint) for taint in tainted_record.stack[-3])
-                if index in self.overflows:
-                    #print("!!!!!!!")
-                    #print("!!!!!!!")
-                    #print("yolo")
-                    #print("!!!!!!!")
-                    #print("!!!!!!!")
-                    return self.overflows[index], "overflow"
-                if index in self.underflows:
-                    return self.underflows[index], "underflow"
-        # Check if overflow flows into condition
-        elif current_instruction and current_instruction["op"] in ["LT", "GT", "SLT", "SGT", "EQ"]:
-            #print(tainted_record.stack)
-            if tainted_record and tainted_record.stack:
-                if tainted_record.stack[-1]: # First operand
-                    index = ''.join(str(taint) for taint in tainted_record.stack[-1])
-                    if index in self.overflows:
-                        return self.overflows[index], "overflow"
-                    if index in self.underflows:
-                        return self.underflows[index], "underflow"
-                if tainted_record.stack[-2]: # Second operand
-                    index = ''.join(str(taint) for taint in tainted_record.stack[-2])
-                    if index in self.overflows:
-                        return self.overflows[index], "overflow"
-                    if index in self.underflows:
-                        return self.underflows[index], "underflow"
+            if tainted_record and tainted_record.stack and tainted_record.stack[-2]:
+                for pc in self.overflows:
+                    for var1 in get_vars(tainted_record.stack[-2][0]):
+                        for var2 in self.overflows[pc]:
+                            if var1 == var2:
+                                return pc, "overflow"
+                for pc in self.underflows:
+                    for var1 in get_vars(tainted_record.stack[-2][0]):
+                        for var2 in self.underflows[pc]:
+                            if var1 == var2:
+                                return pc, "underflow"
         return None, None
